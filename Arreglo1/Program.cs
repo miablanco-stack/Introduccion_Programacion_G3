02c2e1441cfff8601e896edda1a2aa7dcbef53bc
miablanco <mia.blanco@uamv.edu.ni>
2026-05-06 09:53:11 -0600

    feat. Add proyecto arreglo1

---
+++ Arreglo1/Program.cs
@@ -0,0 +1,49 @@
+﻿/* Almacenar 10 edades en un arreglo, luego calcular el promedio de edades, el maximo */
+//Crear arreglo de edades
+int[] edades = new int[10];
+//Pedir edades
+for (int i = 0; i < edades.Length; i++)
+{
+    try
+    {
+
+        Console.WriteLine($"Digita la edad {i + 1}:");
+        edades[i] = int.Parse(Console.ReadLine());
+
+        if (edades[i] < 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("La edad no puede ser negativa.");
+            i--; // Reintentar la misma posición
+            Console.ResetColor();
+        }
+    }
+    catch
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Por favor, ingrese un número válido.");
+        i--; // Reintentar la misma posición
+        Console.ResetColor();
+    }
+}
+
+//Calcular promedio
+int suma = 0, maximo = edades[0], minimo = edades[0];
+
+foreach(int edad in edades)
+{
+    suma += edad;
+    if (edad > maximo) 
+    {
+        maximo = edad;
+    }
+    if (edad < minimo)
+    {
+        minimo = edad;
+    }
+}
+double promedio = (double)suma / edades.Length;
+//Mostrar resultados
+Console.WriteLine($"El promedio de edades es: {promedio}");
+Console.WriteLine($"La edad máxima es: {maximo}");
+Console.WriteLine($"La edad mínima es: {minimo}");
--- Switch/for1/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿Console.WriteLine("Hello, World!");
-int i;
-for (i=100; i >= 80; i--)
-{
-    Console.WriteLine(i);
-}
