52c037e085bed56282b4a595d97ecb820cc510e9
miablanco <mia.blanco@uamv.edu.ni>
2026-05-10 22:44:31 -0600

    feat. Add proyecto MagnitudVector

---
+++ MagnitudVector/Program.cs
@@ -0,0 +1,12 @@
+﻿int [] direccion =  {5, 20, 5, 9, 7};
+double sumadecuadrados =  0;
+double magnitud;
+
+for (int i = 0; i < direccion.Length; i++)
+{
+    sumadecuadrados += Math.Pow (direccion[i], 2);
+}
+
+magnitud = Math.Sqrt(sumadecuadrados);
+Console.WriteLine($"La magnitud del vector es {magnitud} ");
+
