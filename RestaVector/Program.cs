cd1a5ab5edfc63a03afb8976657c440e4431d989
miablanco <mia.blanco@uamv.edu.ni>
2026-05-10 20:57:28 -0600

    feat. Add proyecto RestaVector

---
+++ RestaVector/Program.cs
@@ -0,0 +1,48 @@
+﻿int [] vector1 = new int [5];
+int [] vector2 = new int [5];
+int [] restaVector = new int [5];
+
+Console.WriteLine("Ingresa los valores del primer vector");
+for (int i = 0;  i < vector1.Length; i++)
+{
+    try
+    {
+        Console.WriteLine($"Ingrese el valor {i + 1}");
+        vector1[i] = int.Parse(Console.ReadLine());
+    }
+    catch (FormatException)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Por favor ingrese un numero valido");
+        i--; //Decrementar el indice para volver a solicitar el valor
+        Console.ResetColor();
+    }
+}
+Console.WriteLine("Ingresa los valores del segundo vector");
+for (int i = 0; i < vector2.Length; i++)
+{
+    try
+    {
+        Console.WriteLine($"Ingrese el Valor {i + 1}");
+        vector2[i] = int.Parse(Console.ReadLine());
+    
+    }
+    catch (FormatException)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Por favor ingrese un numero valido");
+        i--; //Decrementar el indice para volver a solicitar el valor
+        Console.ResetColor();
+    }
+}
+//Restar Vectores
+for (int i = 0; i < restaVector.Length; i++)
+{
+    restaVector[i] = vector1[i] - vector2[i];
+}
+// Mostrar arreglo resultante
+for (int i = 0; i < restaVector.Length; i++)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"El rsultado de la diferencia entre el vector uno y el vector dos en la posicion {i + 1} es {restaVector[i]}");
+}
