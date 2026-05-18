18019b4c140cbc309c461cf5cc6af3143f4b2aba
miablanco <mia.blanco@uamv.edu.ni>
2026-05-17 22:02:07 -0600

    feat. add proyecto Matriz1

---
+++ Matriz1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Matriz 3 x 3
+int[,] matriz = new int [3 ,3];
+
+Console.WriteLine("Digite los elementos de matriz 3x3; ");
+for(int f = 0; f < 3; f++)
+{
+    for (int c = 0; c < 3; c++)
+    {
+        Console.Write($"Elemento [{f}, {c}]");
+        matriz[f,c] = int.Parse(Console.ReadLine()!);
+        
+    }
+}
+Console.WriteLine("Mostrar matriz");
+for (int f = 0; f < 3; f++)
+{
+    for (int c = 0; c < 3; c++)
+    {
+        if(matriz[f,c] % 2 == 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Green; 
+        }
+         else
+        {
+            Console.ForegroundColor = ConsoleColor.Red; 
+        }
+        if (matriz[f,c] < 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow; 
+        }
+        Console.Write($"{matriz[f , c] } ");
+    }
+    Console.ResetColor();
+    Console.WriteLine();
+}
